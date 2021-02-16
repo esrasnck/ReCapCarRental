27f9c516e6f823a7c61778afca7178bacb982997
esrasnck <esrasancak@hotmail.com>
2021-02-16 12:52:36 +0300

    Devamı Gelecek

--- Bussiness/Abstract/IUserService.cs
+++ Bussiness/Abstract/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,10 +8,13 @@ namespace Bussiness.Abstract
 {
     public interface IUserService
     {
-    
-
-        void Add(User user);   // todo: Iresult'a çek
-                                     // todo : IDataResult a çek
-        User GetByMail(string email); // daha önce bir kullanıcı olup olmadığını tespit etmek için gerekli iş kodu
+       IDataResult<User> GetByCustomerId(int customerId);
+       IDataResult<User> GetByUserId(int userId);
+       IDataResult<User> GetAll();
+       IResult Delete(User user);
+       IResult Update(User user);
+       IResult Add(User user);   // todo: Iresult'a çek
+                                  // todo : IDataResult a çek
+       IDataResult<User> GetByMail(string email); // daha önce bir kullanıcı olup olmadığını tespit etmek için gerekli iş kodu => çözmem gerek :(
     }
 }
